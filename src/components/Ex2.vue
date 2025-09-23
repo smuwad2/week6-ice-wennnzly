b875675627d2c64d3ffdb8f62be2e00357c0b030
wennnzly <arwen.tan.2024@computing.smu.edu.sg>
2025-09-23 11:33:13 +0800

    update to ex4

--- src/components/Ex2.vue
+++ src/components/Ex2.vue
@@ -13,5 +13,11 @@
 <template>
 
     <!-- add code here -->
+     <img v-bind:src="myImg" v-bind:width="width" v-bind:height="height"></img>
+     <!-- 
+     this works too! without v-bind (':' works as v-bind )
+     <img v-bind:src="myImg" :width="width" :height="height"></img>
+     : is a short-hand notation of v-bind
+     -->
    
 </template>
--- src/components/Ex4.vue
+++ src/components/Ex4.vue
@@ -3,10 +3,33 @@
         data() {
             return {
                 // Add code here
+                id : "demo1",
+                isBlue : true,
+                color : "blueBox",
+                button : "btn-primary"
             }
         },
         methods: { 
    			// Add code here
+            //toChange(isBlue){
+                // if (isBlue){
+                //    console.log(isBlue);
+                //    this.isBlue=false;
+                //    isBlue=false;
+                //    this.color="redBox";
+                //}
+                //else{
+                //    this.isBlue=true;
+                //    isBlue=false;
+                //    this.color="blueBox";
+                //}
+
+            //}
+
+            changeColor(){
+                this.blueBox=!this.blueBox;
+                this.redBox=!this.bl
+            }
 		}
     }
 </script>
@@ -14,12 +37,22 @@
 <template>
     
     <!-- Modify code here -->
-    <div class="container">
-        <div v-bind:id="id" class="m-2">
+    <!--<div   class="container">
+        <div v-bind:class="color" v-on:click="toChange" v-bind:id="id" class="m-2">
             div ID : {{id}} 
         </div>
         
-        <button type="button">Change Color</button>
+        <button type="button" v-bind:class="color" v-on:click="toChange">Change Color</button>
+    </div>
+    -->
+
+    <!-- Modify code here -->
+    <div   class="container">
+        <div v-bind:id="id" class="m-2" :class="{'blueBox' : blueBox,'redBox' : !blueBtn}">
+            div ID : {{id}} 
+        </div>
+        
+        <button type="button" :class="{'btn btn-primary' : blueBtn, 'btn btn-danger' : !blueBtn}" v-on:click = "changeColor()">Change Color</button>
     </div>
 
 </template>
